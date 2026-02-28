0eb3d18d1aeb4c5a3de454f2fd4dfc2c16526054
rahe01 <rahatrahe01@gmail.com>
2026-03-01 00:14:57 +0600

    cookie jwt token file created

--- src/module/auth/auth.routes.ts
+++ src/module/auth/auth.routes.ts
@@ -7,7 +7,7 @@ import authController from "./auth.controller";
 const router = Router();
 
 
-router.post("/register", authController.register);
+router.post("/registeruser", authController.register);
 router.post("/login", authController.login);
 
 export const authRoutes = router;
