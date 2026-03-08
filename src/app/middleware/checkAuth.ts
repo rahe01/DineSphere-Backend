cb572f4184be32f2f71ca04863a142628b1a0d3f
rahe01 <rahatrahe01@gmail.com>
2026-03-08 22:45:23 +0600

    error handler

--- src/app/middleware/checkAuth.ts
+++ src/app/middleware/checkAuth.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { Role } from "../../generated/prisma/enums";
+import { Role, UserStatus } from "../../generated/prisma/enums";
 import { CookieUtils } from "../utils/cookie";
 import { prisma } from "../lib/prisma";
 
@@ -40,7 +40,39 @@ export const checkAuth = (...authRole: Role[]) => async (req: Request, res: Resp
             if(sessionExists && sessionExists.user){
                 
                 const user = sessionExists.user;
-                console.log(user);
+                const now = new Date();
+
+                const createdAt =new Date(sessionExists.createdAt);
+                const expiredAt = new Date(sessionExists.expiresAt);
+
+                const sessionLifeTime = expiredAt.getTime() - createdAt.getTime();
+                const timeRemaining = expiredAt.getTime() - now.getTime();
+                const percentRemaining = (timeRemaining / sessionLifeTime) * 100;
+
+
+
+                
+
+                console.log(sessionLifeTime, "Session lifetime" );
+                console.log(timeRemaining, "Time remaining" );
+                console.log(percentRemaining, "Percent remaining" );
+
+
+                if(percentRemaining < 99){
+                    res.setHeader("X-Session-Refresh", "true");
+                    res.setHeader("X-Session-Expires-At" , expiredAt.toISOString());
+                    res.setHeader("X-Session-Percent-Remaining" , timeRemaining.toString());
+                    console.log("Session expires soon ");
+
+                }
+
+                if(user.status === UserStatus.BLOCKED || user.status === UserStatus.DELETED || user.status === UserStatus.SUSPENDED){
+                    throw new Error("Unauthorized: User account is blocked, deleted, or suspended.");
+                }
+
+                
+
+
             }
 
 
